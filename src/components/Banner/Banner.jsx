00fa2710e7980d603817e443420b33e2307c4813
TanveerAhmed4545 <tanveer8507@gmail.com>
2024-07-13 02:47:52 +0600

    add banner components

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,5 +1,10 @@
+import background from "../../assets/images/Background.png";
 const Banner = () => {
-  return <div></div>;
+  return (
+    <div className="">
+      <img className="h-48 md:h-56 lg:h-64 w-full" src={background} />
+    </div>
+  );
 };
 
 export default Banner;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -16,7 +16,7 @@ const NavBar = () => {
   };
 
   return (
-    <nav className="bg-primary text-white border-b border-gray-200">
+    <nav className="bg-primary text-white ">
       <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-20">
           <div className="flex items-center">
